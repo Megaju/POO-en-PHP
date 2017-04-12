3c03364add4a61c866d8adc523bb470ec12c5797
Megaju <megaju@kijewoku.fr>
2017-04-12 14:53:42 +0200

    nouveau test sur la génération de formulaire

--- form/form.php
+++ form/form.php
@@ -5,18 +5,30 @@ class Form {
     private $data = [];
     public $surround = 'p';
 
-    public function __construct($data) {
+    // constructeur
+    public function __construct($data = array()) {
         $this->data = $data;
     }
 
+    // fonction pour entrourer nos balise input d'une autre balise, 'p' pour notre exemple.
     private function surround($html) {
         return "<{$this->surround}>{$html}</{$this->surround}>";
     }
 
-    public function input($name) {
-        return $this->surround('<input type="text" name="'.$name.'">');
+    // récupérer la valeur de l'index souhaité
+    private function getValue($index) {
+        return isset($this->data[$index]) ? $this->data[$index] : null;
     }
 
+    // fonction pour créer nos input
+    // getValue() nous sert à récupérer la valeur de l'index afin d'afficher cette valeur en préremplissant le champ, et rien, si rien n'a été précisé dans la vue.
+    public function input($name) {
+        return $this->surround(
+            '<input type="text" name="'.$name.'" value="'.$this->getValue($name).'">'
+        );
+    }
+
+    // fonction pour créer notre boutton submit
     public function submit() {
         return $this->surround('<button type="submit">Envoyer</button>');
     }
--- form/index.php
+++ form/index.php
@@ -8,6 +8,17 @@ $form = new Form(array(
     'username' => 'Megaju'
 ));
 
+// REMARQUE (prendre l'habitude de garder les echo pour la vue et les return dans nos class)
+
 echo $form->input('username');
 echo $form->input('password');
 echo $form->submit();
+
+$second_form = new Form();
+
+echo $second_form->input('newformtest');
+echo $second_form->input('newformtest');
+echo $second_form->input('newformtest');
+echo $second_form->input('newformtest');
+echo $second_form->input('newformtest');
+echo $second_form->submit();
