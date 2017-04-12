65c5479ce33819d8bdb18cbbeea9c944c040ec20
Megaju <megaju@kijewoku.fr>
2017-04-12 15:44:48 +0200

    test constante

--- form/text.php
+++ form/text.php
@@ -10,14 +10,19 @@ class Text {
     private static $suffix = " €";
 
     /**
+     * @var string Juste pour montrer ce qu'est une constante
+     */
+     const CENTS = " cents";
+
+    /**
      * @param $int int Ajoute un 0 si chiffre inférieur à 10
      * @return string
      */
     public static function withZero($int) {
         if ($int < 10) {
-            return '0' . $int . self::$suffix;
+            return '0' . $int . self::$suffix . self::CENTS;
         } else {
-            return $int . self::$suffix;
+            return $int . self::$suffix . self::CENTS;
         }
     }
 
